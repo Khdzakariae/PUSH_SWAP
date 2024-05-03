825c67b09a7f917c56de51717310325686d2008e
Khdzakariae <elkhadirzakaria48@gmail.com>
2024-05-03 11:30:11 +0100

    Fri May  3 11:30:11 +01 2024

--- sources/utils.c
+++ sources/utils.c
@@ -6,7 +6,7 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 18:56:48 by zel-khad          #+#    #+#             */
-/*   Updated: 2024/05/03 11:22:27 by zel-khad         ###   ########.fr       */
+/*   Updated: 2024/05/03 11:29:16 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,13 +46,14 @@ void	_push(t_stack **a, int ac, char **av)
 		if (!*numbers)
 			exit_(numbers, a);
 		i = 0;
-		while (numbers[++i] != NULL)
+		while (numbers[i] != NULL)
 		{
 			number = ft_atoi(numbers[i]);
 			free(numbers[i]);
 			if ((number > INT_MAX) || (number < INT_MIN))
 				exit_(numbers, a);
 			add_node(&(*a), number);
+			i++;
 		}
 		free(numbers);
 	}
